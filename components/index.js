2bd0bd4490ba691acaae32e418a8be94f52bd12f
Stephen Mckellar <wwqrd@users.noreply.github.com>
2017-06-26 12:25:47 +0100

    Update README.md

---
+++ components/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.animation = exports.Node = exports.SideMenu = exports.TextInput = exports.Icon = exports.Button = undefined;
+
+var _Button2 = require('./Button');
+
+var _Button3 = _interopRequireDefault(_Button2);
+
+var _Icon2 = require('./Icon');
+
+var _Icon3 = _interopRequireDefault(_Icon2);
+
+var _TextInput2 = require('./Inputs/TextInput');
+
+var _TextInput3 = _interopRequireDefault(_TextInput2);
+
+var _SideMenu2 = require('./SideMenu');
+
+var _SideMenu3 = _interopRequireDefault(_SideMenu2);
+
+var _Node2 = require('./Node');
+
+var _Node3 = _interopRequireDefault(_Node2);
+
+var _animation2 = require('./animation');
+
+var _animation3 = _interopRequireDefault(_animation2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Button = _Button3.default;
+exports.Icon = _Icon3.default;
+exports.TextInput = _TextInput3.default;
+exports.SideMenu = _SideMenu3.default;
+exports.Node = _Node3.default;
+exports.animation = _animation3.default;
+//# sourceMappingURL=index.js.map
