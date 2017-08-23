acb5ab9145bc113693b66902d4c71d6df1a331a7
Steve Mckellar <steve@wwqrd.uk>
2017-08-23 14:39:33 +0100

    WIP

--- src/components/Inputs/TextInput.js
+++ src/components/Inputs/TextInput.js
@@ -1,100 +1,47 @@
+/* eslint-disable */
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import InputLabel from './InputLabel';
 
-class TextInput extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-    errorText: PropTypes.node,
-    disabled: PropTypes.bool,
-    name: PropTypes.string,
-    isNumericOnly: PropTypes.bool,
-    label: PropTypes.string,
-    onBlur: PropTypes.func,
-    onChange: PropTypes.func,
-    onFocus: PropTypes.func,
-    placeholder: PropTypes.string,
-    validator: PropTypes.func,
-    value: PropTypes.any,
-  }
+const TextInput = ({
+  input,
+  meta,
+  className,
+  label,
+  isNumericOnly,
+  placeholder,
+  ...rest
+}) => {
 
-  state = {
-    hasValue: false,
-    isFocused: false,
-  }
+  const inputLabelClassName = cx({
+    input__label: true,
+    'input__label--active': meta.active,
+  });
 
+  const showPlaceholder = (meta.active || meta.dirty) ? placeholder : null;
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.hasOwnProperty('value')) {
-      let hasValue = Boolean(nextProps.value);
-      if (nextProps.validator) {
-        hasValue = nextProps.validator(nextProps.value);
-      }
-      this.setState({ hasValue });
-    }
-  }
-
-  handleBlur = (e) => {
-    this.setState({ isFocused: false });
-    if (this.props.onBlur) {
-      this.props.onBlur(e);
-    }
-  }
-
-  handleFocus = (e) => {
-    if (this.props.disabled) {
-      return;
-    }
-    this.setState({ isFocused: true });
-    if (this.props.onFocus) {
-      this.props.onFocus(e);
-    }
-  }
-
-  render() {
-    const {
-      className,
-      errorText,
-      name,
-      label,
-      onChange,
-      isNumericOnly,
-      placeholder,
-      value,
-      ...rest
-    } = this.props;
-
-    const inputLabelClassName = cx({
-      input__label: true,
-      'input__label--active': this.state.hasValue,
-    });
-
-    const showPlaceholder = (this.state.isFocused && !this.state.hasValue) ? placeholder : null;
-
-    return (
-      <div className="input__container">
-        <input
-          className={cx(['input', className])}
-          name={name}
-          type={isNumericOnly ? 'tel' : 'text'}
-          onBlur={this.handleBlur}
-          onChange={onChange}
-          onFocus={this.handleFocus}
-          placeholder={showPlaceholder}
-          {...rest}
-        />
-        <InputLabel
-          active={this.state.hasValue}
-          name={name}
-          label={label}
-          className={inputLabelClassName}
-          errorText={errorText}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="input__container">
+      <input
+        className={cx(['input', className])}
+        autoComplete="off"
+        type={isNumericOnly ? 'number' : 'text'}
+        placeholder={showPlaceholder}
+        {...rest}
+        {...input}
+      />
+      <InputLabel
+        active={meta.active}
+        name={input.name}
+        label={label}
+        className={inputLabelClassName}
+        errorText={meta.error}
+      />
+    </div>
+  );
+};
 
 export default TextInput;
